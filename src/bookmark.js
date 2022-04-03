2bb44d7eaa2dd64641bb3e4db49deb71adcc140e
Erik Scheurer <erik.scheurer@gmx.de>
2022-04-03 18:52:10 +0200

    Remove Chrome and add code to bookmarks.js

--- src/bookmark.js
+++ src/bookmark.js
@@ -1,21 +1,3 @@
-# PDF Ref Preview
-
-Preview internal links in PDFs on mouse hover.
-
-<p align="center">
-  <img title="Demo" src="https://raw.githubusercontent.com/belinghy/PDFRefPreview/assets/assets/demo.gif">
-</p>
-
-The script works by following internal links to the target page and location, and render the view in a separate canvas. Some PDFs may not work because the internal links are broken.
-
-This script only works if the viewer is [PDF.js](https://github.com/mozilla/pdf.js/).
-
-## Firefox
-
-PDF.js is the default viewer in Firefox. To install the script, create a bookmark and add the code below to the `URL` field. Use the bookmark to toggle enable/disable preview after a PDF has been loaded.
-
-<script scr=""></script>
-```js
 javascript:(
     async () => { 
         const e = window.PDFViewerApplication, 
@@ -80,4 +62,3 @@ javascript:(
                 })
         }
      e._previewing = !1, t.addEventListener("mouseover", o), e._previewHandler = o })();
-```
